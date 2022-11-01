f925f12e5648abb6e082f009ce9362ce2e93ccb3
Lea Broennimann <lbroennimann@binfservms01.unibe.ch>
2022-11-01 11:31:42 +0100

    Script for Quality Check

---
+++ scripts/quality_checks.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1000M
+#SBATCH --time=01:10:00
+#SBATCH --job-name=quality_checks
+#SBATCH --mail-user=lea.broennimann@students.unibe.ch
+#SBATCH --mail-type=begin,end
+#SBATCH --output=/data/courses/rnaseq_course/breastcancer_de/lea_broe/quality_checks/quality_checks_%j.o
+#SBATCH --error=/data/courses/rnaseq_course/breastcancer_de/lea_broe/quality_checks/error_quality_checks_%j.e
+
+## Change directories to where the fastq files are located
+cd /data/courses/rnaseq_course/breastcancer_de/reads
+
+## Load modules required for script commands
+module load UHTS/Quality_control/fastqc/0.11.9
+
+## Run FASTQC
+fastqc -o /data/courses/rnaseq_course/breastcancer_de/lea_broe/quality_checks/ -t 2 *.fq
