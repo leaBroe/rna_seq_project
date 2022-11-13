1ceaeb1330eb36bc987d5a9a123e9e8fdfd4b5f7
Lea Broennimann <lbroennimann@binfservms01.unibe.ch>
2022-11-13 16:28:57 +0100

    updated map_reads_to_genome_hisat2.sh and created convert_sam_to_bam_samtools.sh

---
+++ scripts/convert_sam_to_bam_samtools.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4000M
+#SBATCH --time=02:00:00
+#SBATCH --job-name=convert_sam_to_bam
+#SBATCH --mail-user=lea.broennimann@students.unibe.ch
+#SBATCH --mail-type=begin,end
+#SBATCH --output=/data/users/lbroennimann/convert_to_bam_%j.o
+#SBATCH --error=/data/users/lbroennimann/convert_to_bam_%j.e
+
+cd /data/users/lbroennimann/rna_seq_project/map_reads
+
+module add UHTS/Analysis/samtools/1.10
+
+for i in HER21 HER22 HER23 NonTNBC1 NonTNBC2 NonTNBC3 Normal1 Normal2 Normal3 TNBC1 TNBC2 TNBC3;
+do samtools view -hbS /data/users/lbroennimann/rna_seq_project/map_reads/mapped_reads_${i}.sam > mapped_reads_${i}.bam;
+done
+
+
