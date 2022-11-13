a1db42a7cf3eebcc2f5508f76da4186d785ce138
Lea Broennimann <lbroennimann@binfservms01.unibe.ch>
2022-11-13 22:58:56 +0100

    updated convert_sam_to_bam_samtools.sh, created sort_bam_files_samtools

--- scripts/convert_sam_to_bam_samtools.sh
+++ scripts/convert_sam_to_bam_samtools.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4000M
-#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=42000M
+#SBATCH --time=05:00:00
 #SBATCH --job-name=convert_sam_to_bam
 #SBATCH --mail-user=lea.broennimann@students.unibe.ch
 #SBATCH --mail-type=begin,end
@@ -14,7 +14,7 @@ cd /data/users/lbroennimann/rna_seq_project/map_reads
 module add UHTS/Analysis/samtools/1.10
 
 for i in HER21 HER22 HER23 NonTNBC1 NonTNBC2 NonTNBC3 Normal1 Normal2 Normal3 TNBC1 TNBC2 TNBC3;
-do samtools view -hbS /data/users/lbroennimann/rna_seq_project/map_reads/mapped_reads_${i}.sam > mapped_reads_${i}.bam;
+do samtools view -hbS mapped_reads_${i}.sam > mapped_reads_${i}.bam;
 done
 
 
