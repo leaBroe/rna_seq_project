21c2eb8485661485b19e4fa4f6c6b221e8e7ab57
leaBroe <116747293+leaBroe@users.noreply.github.com>
2022-11-17 17:41:34 +0100

    parallelised feature_counts.sh
    added minimal mapping quality (25)

--- scripts/feature_counts.sh
+++ scripts/feature_counts.sh
@@ -8,13 +8,15 @@
 #SBATCH --mail-type=begin,end
 #SBATCH --output=/data/users/lbroennimann/feature_counts_%j.o
 #SBATCH --error=/data/users/lbroennimann/feature_counts_%j.e
+#SBATCH --array=0-11%6
 
 cd /data/users/lbroennimann/rna_seq_project/map_reads
 
 module load UHTS/Analysis/subread/2.0.1;
 
-for i in HER21 HER22 HER23 NonTNBC1 NonTNBC2 NonTNBC3 Normal1 Normal2 Normal3 TNBC1 TNBC2 TNBC3;
-do featureCounts -p -T 4 -a Homo_sapiens.GRCh38.108.gtf -G Homo_sapiens.GRCh38.dna.primary_assembly.fa -o ${i}_featureCounts_output.txt sorted_${i}.bam;
-done
+samples=('HER21' 'HER22' 'HER23' 'NonTNBC1' 'NonTNBC2' 'NonTNBC3' 'Normal1' 'Normal2' 'Normal3' 'TNBC1' 'TNBC2' 'TNBC3')
+
+featureCounts -p -Q 25 -a Homo_sapiens.GRCh38.108.gtf -G Homo_sapiens.GRCh38.dna.primary_assembly.fa -o ${sample[$SLURM_ARRAY_TASK_ID]}_featureCounts_output.txt sorted_${sample[$SLURM_ARRAY_TASK_ID]}.bam
+
 
 
