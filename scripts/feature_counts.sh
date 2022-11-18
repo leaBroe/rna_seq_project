fab34886d3b2fc01634912d6556bdde4244580ba
leaBroe <116747293+leaBroe@users.noreply.github.com>
2022-11-18 13:50:07 +0100

    Update feature_counts.sh

--- scripts/feature_counts.sh
+++ scripts/feature_counts.sh
@@ -16,7 +16,7 @@ module load UHTS/Analysis/subread/2.0.1;
 
 samples=('HER21' 'HER22' 'HER23' 'NonTNBC1' 'NonTNBC2' 'NonTNBC3' 'Normal1' 'Normal2' 'Normal3' 'TNBC1' 'TNBC2' 'TNBC3')
 
-featureCounts -p -Q 25 -a Homo_sapiens.GRCh38.108.gtf -G Homo_sapiens.GRCh38.dna.primary_assembly.fa -o ${sample[$SLURM_ARRAY_TASK_ID]}_featureCounts_output.txt sorted_${sample[$SLURM_ARRAY_TASK_ID]}.bam
+featureCounts -p -C -s 0 -Q 25 -T 4 --tmpDir $SCRATCH -a Homo_sapiens.GRCh38.108.gtf -G Homo_sapiens.GRCh38.dna.primary_assembly.fa -o ${sample[$SLURM_ARRAY_TASK_ID]}_featureCounts_output.txt sorted_${sample[$SLURM_ARRAY_TASK_ID]}.bam
 
 
 
