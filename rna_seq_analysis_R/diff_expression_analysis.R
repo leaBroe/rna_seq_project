c643a7bf3405274c67eaa4eb4494461a1070b0d4
leaBroe <lea.broennimann@students.unibe.ch>
2022-12-11 15:53:07 +0100

    updated R analysis (prettier plots)

--- rna_seq_analysis_R/diff_expression_analysis.R
+++ rna_seq_analysis_R/diff_expression_analysis.R
@@ -11,6 +11,9 @@ BiocManager::install("DESeq2")
 library("DESeq2")
 
 library("pheatmap")
+library("ggplot2")
+library("RColorBrewer")
+
 
 # Read count matrix and change column names
 count_matrix <- read.csv("/Users/leabroennimann/Desktop/Master_Bioinformatik/1._Semester/RNA_Seq/feature_counts_2nd_output/gene_matrix_count.csv", header = T, row.names = 1)
@@ -20,6 +23,8 @@ colnames(count_matrix) <- c("HER21", "Normal2", "NonTNBC3", "TNBC1", "NonTNBC2",
 sample_info <- data.frame(c("HER21", "Normal2", "NonTNBC3", "TNBC1", "NonTNBC2", "Normal3", "TNBC2", "HER22", "Normal1", "HER23", "TNBC3", "NonTNBC1"), 
                           c("HER2", "Normal", "NonTNBC", "TNBC", "NonTNBC", "Normal", "TNBC", "HER2", "Normal", "HER2", "TNBC", "NonTNBC" ))
 colnames(sample_info) <- c("sample", "type")
+
+sample_info <- sample_info[match(colnames(countDataMatrix), as.character(sample_info$sample)), ]
 
 # Turn count matrix from dataframe to matrix
 countDataMatrix <- as.matrix(count_matrix[ , ])
@@ -38,18 +43,39 @@ vsd <- vst(dds2, blind=TRUE)
 rlog <- rlog(dds2, blind=TRUE)
 
 # Create heatmap
-select <- order(rowMeans(counts(dds2,normalized=FALSE)), decreasing=TRUE)[1:20]
+select <- order(rowMeans(counts(dds2,normalized=FALSE)), decreasing=TRUE)[1:50]
 
 df <- as.data.frame(colData(dds2)[,"type"])
 
 pheatmap(assay(vsd)[select,], cluster_rows=FALSE, show_rownames=FALSE, cluster_cols=FALSE, main="Heatmap of the count matrix")
 ?pheatmap
 
+# Heatmap of the sample-to-sample distances
+
+sampleDists <- dist(t(assay(vsd)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- paste(vsd$condition, vsd$type, sep="-")
+colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette( rev(brewer.pal(9, "Oranges")) )(255)
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
+
 # Create PCA plots with different transformations
-plotPCA(vsd, intgroup="type")
+pcaData <- plotPCA(vsd, intgroup="type", returnData=TRUE)
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+ggplot(pcaData, aes(PC1, PC2, color=type)) +
+  geom_point(size=3) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+  coord_fixed()+
+  labs(title="PCA plot of vst transformed data", colour="Group") +
+  theme_light()
+
+
 plotPCA(rlog, intgroup="type")
 
-sample_info <- sample_info[match(colnames(countDataMatrix), as.character(sample_info$sample)), ]
 
 # Differential expression analysis
 resultsNames(dds2)
@@ -64,9 +90,11 @@ resultsNames(dds2)
 
 p_values_HER2[which(p_values_HER2 < 0.05)]
 
+sum(results_HER2$padj < 0.05, na.rm=TRUE)
 
+sum(results_TNBC$padj < 0.05, na.rm=TRUE)
 
-
+sum(results_NonTNBC$padj < 0.05, na.rm=TRUE)
 
 
 
